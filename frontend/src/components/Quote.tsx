73a94c4d98fe5e3db02f7ade5380849a84d022d2
Samkit <samkitsoni09@gmail.com>
2024-08-25 20:20:56 +0530

    Added Signin and Signup Routes

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,18 @@
+export const Quote = () => {
+    return <div className="bg-slate-200 h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+            <div className="max-w-lg">
+                <div className="text-3xl font-bold">
+                    "Close the door. Write with no one looking over your shoulder. Don’t try to figure out what other people want to hear from you; figure out what you have to say. 
+                    It’s the one and only thing you have to offer."
+                </div>
+                <div className="max-w-md text-right text-xl semi-bold italic mt-4">
+                    ~ Barbara Kingsolver
+                </div>
+                <div className="max-w-md text-right text-s italic">
+                    American novelist and essayist
+                </div>
+            </div>
+        </div>
+    </div>
+}
